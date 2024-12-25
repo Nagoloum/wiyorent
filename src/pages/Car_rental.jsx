c81024180c7d51eaf67d9d453cfd1fdd037a2444
Nagolouml <landhack049@gmail.com>
2024-12-25 11:42:02 +0100

    modif 1

--- src/pages/Car_rental.jsx
+++ src/pages/Car_rental.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../composants/Footer';
 import Header from '../composants/Header';
-import Car from '../composants/car';
+import Car from '../composants/Cars';
 
 function Car_rental() {
   return (
